e319164d555d0e2eb6bc44a273147305d7cc41fe
SEWsam <49174351+SEWsam@users.noreply.github.com>
2021-08-09 11:48:37 -0500

    bump release date
    i guess im releasing off the refactor branch now considering i'm not yet done

--- DOOMdumper/DOOMdumper.hpp
+++ DOOMdumper/DOOMdumper.hpp
@@ -38,7 +38,7 @@ namespace fs = std::filesystem;
 namespace winrt { using namespace Windows::ApplicationModel; }
 
 // Global Version constants
-inline constexpr char Updated[] = "2021-08-08";
+inline constexpr char Updated[] = "2021-08-09";
 inline constexpr uint64_t Min_Free = 91268055040; // Minimum space, in bytes, required to dump DOOM Eternal
 
 extern const GameVersion game_version;
