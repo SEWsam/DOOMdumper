8e349758d35ad63e8f4119ff2322c78e70784a45
SEWsam <49174351+SEWsam@users.noreply.github.com>
2021-08-11 01:04:11 -0500

    working prototype
    launches with no prompt if remote version matches. if local version is outdated, the user will be asked to update or launch the game. update function does nothing so far.

---
+++ DOOMdumperLauncher/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+namespace DOOMdumperLauncher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // this doesn't do much different than the default setup. just placeholder code really
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            bool waitpls = false;
+            while (waitpls) { continue; }
+
+            bool skip_updates = bool.Parse(ConfigurationManager.AppSettings.Get("SkipUpdates"));
+            if (e.Args != null && e.Args.Length > 0)
+            {
+                for (int i = 0; i < e.Args.Length; i++)
+                {
+                    if(e.Args[i].Equals("--skip-updates"))
+                    {
+                        skip_updates = true;
+                    }
+                }
+            }
+            // todo: exception handling if config is invalid
+            bool update_available = false;
+            if (!skip_updates)
+            {
+                Launcher.latest_version = Launcher.GetLatestVersion();
+                update_available = (Launcher.latest_version > Launcher.dumped_version);
+            }
+            if (update_available)
+            {
+                var window = new MainWindow();
+                window.ShowDialog();
+            }
+            else
+            {
+                Launcher.StartDEternal();
+                Shutdown(0);
+                return;
+            }
+        }
+    }
+}
---
+++ DOOMdumperLauncher/Launcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Configuration;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOOMdumperLauncher
+{
+    /// <summary>
+    /// Launcher class which houses methods and variables used by the launcher
+    /// </summary>
+    public static class Launcher
+    {
+        public static Version latest_version;
+        public static Version dumped_version = new Version(ConfigurationManager.AppSettings.Get("GameVersion"));
+
+        /// <summary>
+        /// Start a DOOMEternalx64vk.exe process, 
+        /// assuming the exe is in the same dir as the launcher exe
+        /// </summary>
+        public static void StartDEternal(string args="")
+        {
+            string exepath = Process.GetCurrentProcess().MainModule.FileName;
+            string exedir = Path.GetDirectoryName(exepath);
+
+            string path = Path.Combine(exedir, "DOOMEternalx64vk.exe");
+            Process.Start(path, args);
+        }
+
+        public static Version GetLatestVersion()
+        {
+            // todo: this url should be in app config
+            string url = "https://sewsam.github.io/deternal_version";
+            string version_string;
+            using (var webclient = new WebClient())
+            {
+                version_string = webclient.DownloadString(url);
+            }
+
+            var version = new Version(version_string);
+            return version;
+        }
+
+        public static void UninstallDEternal()
+        {
+            return;
+        }
+    }
+}
