a8ed83300c6aae015bf3339b606df4cf2c63b08e
SEWsam <49174351+SEWsam@users.noreply.github.com>
2021-09-03 08:50:36 -0500

    formatting

--- DOOMdumperLauncher/Launcher.cs
+++ DOOMdumperLauncher/Launcher.cs
@@ -148,9 +148,9 @@ namespace DOOMdumperLauncher
             [Flags]
             public enum FileShareFlags : uint
             {
-                FILE_SHARE_READ = 0x00000001,
-                FILE_SHARE_WRITE = 0x00000002,
-                FILE_SHARE_DELETE = 0x00000004
+                FILE_SHARE_READ    =  0x00000001,
+                FILE_SHARE_WRITE   =  0x00000002,
+                FILE_SHARE_DELETE  =  0x00000004
             }
 
             [Flags]
