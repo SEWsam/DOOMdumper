5f65761032a129ddc08ab9f532afa14401faff6f
SEWsam <49174351+SEWsam@users.noreply.github.com>
2021-10-26 11:53:49 -0500

    a

--- DOOMdumperLauncher/Launcher.cs
+++ DOOMdumperLauncher/Launcher.cs
@@ -54,45 +54,17 @@ namespace DOOMdumperLauncher
 
         public static void InitUninstaller()
         {
-            /* todo: idea -- requirements for game deletion mode:
-             * first launcher arg must be --uninstall (`-l "--uninstall"`) 
-             * second arg must be --path, with argument after (`-l "--uninstall --path 'E:\Games\DOOMEternal'"`)
-             * Deletion will not proceed if DOOMEternalx64vk.exe is present and if `exedir` matches input path.
-             * 
-             * Two failure outcomes of the above requirements:
-             * 1. If ANY args are passed along with any requirements not met, nothing will happen.
-             * 2. If no args are passed, along with any other requirements not met, we will create 
-             *    a temp copy of this EXE, move it into null with the DELAY_UNTIL_REBOOT flag, then spawn a
-             *    proc of our temp EXE (with the required args) and exit our orig proc. Now, from the
-             *    temp EXE, we will delete the provided directory, de-register DOOM Eternal's UWP Package, and exit.
-             */
             string uninstaller_dir = Path.Combine(Path.GetTempPath(), "DOOMdumperLauncher");
             string uninstaller_path = Path.Combine(uninstaller_dir, "Launcher.exe");
             Directory.CreateDirectory(uninstaller_dir);
 
             File.Copy(exe_path, uninstaller_path);
 
-            var uninstaller_handle = Win32.CreateFileA(
-                uninstaller_path, 
-                (uint)Win32.GenericAccessFlags.GENERIC_READ,
-                (uint)Win32.FileShareFlags.FILE_SHARE_READ | (uint)Win32.FileShareFlags.FILE_SHARE_DELETE, 
-                IntPtr.Zero, 
-                3, 
-                (uint)Win32.FileFlags.FILE_FLAG_DELETE_ON_CLOSE, 
-                IntPtr.Zero);
+            // TODO: It would be nice to remove the temp exe, one day maybe
 
-            /* 
-            // this should work, but it doesn't
-            var uninstaller_stream = new FileStream(uninstaller_path, FileMode.Create, FileAccess.ReadWrite, FileShare.Delete, 4096, FileOptions.DeleteOnClose);
-            using (var src_stream = File.OpenRead(exe_path))
-            {
-                src_stream.CopyTo(uninstaller_stream);
-            }
-            */
-
-            Process.Start(uninstaller_path);
+            string args = "--uninstall --path " + exe_dir;
+            Process.Start(uninstaller_path, args);
             Thread.Sleep(200);
-            uninstaller_handle.Close();
             return;
         }
 
@@ -131,6 +103,9 @@ namespace DOOMdumperLauncher
             return 0;
         }
 
+        /* 
+         * This is useful stuff -- commenting out for now
+         * 
         /// <summary>
         /// Stuff for interacting with C / Unmanaged Win32 API code
         /// </summary>
@@ -177,5 +152,16 @@ namespace DOOMdumperLauncher
 
             
         }
+
+
+            //var uninstaller_handle = Win32.CreateFileA(
+            //    uninstaller_path, 
+            //    (uint)Win32.GenericAccessFlags.GENERIC_READ,
+            //    (uint)Win32.FileShareFlags.FILE_SHARE_READ | (uint)Win32.FileShareFlags.FILE_SHARE_DELETE, 
+            //    IntPtr.Zero, 
+            //    3, 
+            //    (uint)Win32.FileFlags.FILE_FLAG_DELETE_ON_CLOSE, 
+            //    IntPtr.Zero);
+        */
     }
 }
