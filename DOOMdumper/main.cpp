474b63324c96df4d42a47b910bd46f2b870db33b
SEWsam <49174351+SEWsam@users.noreply.github.com>
2021-11-22 16:03:47 -0600

    6.66 rev1 bump

--- DOOMdumper/main.cpp
+++ DOOMdumper/main.cpp
@@ -35,7 +35,7 @@ along with DOOMdumper If not, see <https://www.gnu.org/licenses/>.
 
 namespace fs = std::filesystem;
 
-const GameVersion game_version{1, 0, 13, 0};
+const GameVersion game_version{1, 0, 14, 0};
 
 bool misc_debug = false;
 DebugStream dbgs;
